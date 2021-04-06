d4a50933bb3d0841c6be3799a4fbd0a5fc656d73
Rizki Wahfiudin <rizkiwahfiudin@gmail.com>
2021-04-06 21:14:36 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+IP ME
+==========
+
+[![Live demo](https://rizkiwahfiudin.github.io/ip-me/)
+
+Simple IP Address Information.
+
+#### [Try the DEMO](https://rizkiwahfiudin.github.io/ip-me/)
---
+++ main.js
@@ -0,0 +1,11 @@
+$.get("http://ipinfo.io", function (result) {
+	if(result) {
+	    $("#ip").html("IP : " + result.ip);
+	    $("#city").html("City : " + result.city);
+	    $("#province").html("Province : " + result.region);
+	    $("#country").html("Country : " + result.country);
+	    $("#location").html("Lokasi : " + result.loc);
+	    $("#company").html("Company : " + result.org);
+	    $("#timezone").html("Time Zone : " + result.timezone);
+	}
+}, "jsonp");
